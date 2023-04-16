a4e3add53ad4749b01bed803b92a5fb2df8212c2
gdi21 <57336299+gdi21@users.noreply.github.com>
2023-04-17 00:40:17 +0900

    스크립트 추가
    Feat: 기존에 사용했던 스크립트 파일 추가(일부 제외)

---
+++ FarmingGO/Assets/Scripts/Land.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Land : MonoBehaviour, ITimeTracker
+{
+    public enum LandStatus
+    {
+        Soil, Farmland, Watered
+    }
+
+    public LandStatus landStatus;
+
+    public Material soilMat, farmlandMat, wateredMat;
+    new Renderer renderer;
+
+    //The selection gameobject to enable when the player is selecting the land
+    public GameObject select;
+
+    //Cache the time the land was watared
+    GameTimestamp timeWatered;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get the renderer component
+        renderer = GetComponent<Renderer>();
+
+        //Set the land to soil by default
+        SwitchLandStatus(LandStatus.Soil);
+
+        //Deselect the land by default
+        Select(false);
+
+        //Add this to TimeManager's Listener list
+        TimeManager.Instance.RegisterTracker(this);
+    }
+
+    public void SwitchLandStatus(LandStatus statusToSwitch)
+    {
+        //Set land status accordingly
+        landStatus = statusToSwitch;
+
+        Material materialToSwitch = soilMat;
+
+        //Decide what material to switch to
+        switch (statusToSwitch)
+        {
+            case LandStatus.Soil:
+                //Switch to the soil material
+                materialToSwitch = soilMat;
+                break;
+            case LandStatus.Farmland:
+                //Switch to farmland material 
+                materialToSwitch = farmlandMat;
+                break;
+
+            case LandStatus.Watered:
+                //Switch to watered material
+                materialToSwitch = wateredMat;
+
+                //Cache the time it was watared
+                timeWatered = TimeManager.Instance.GetGameTimestamp();
+                break;
+
+
+
+        }
+
+        //Get the renderer to apply the changes
+        renderer.material = materialToSwitch;
+    }
+
+    public void Select(bool toggle)
+    {
+        select.SetActive(toggle);
+    }
+
+    //When the player presses the interact button while selecting this land
+    public void Interact()
+    {
+        if (landStatus == LandStatus.Farmland)
+        {
+            SwitchLandStatus(LandStatus.Watered);
+        }
+        else
+        {
+            SwitchLandStatus(LandStatus.Farmland);
+        }
+        //Interaction 
+    }
+
+
+    //Checked if 24 hours has passed last watered
+    public void ClockUpdate(GameTimestamp timestamp)
+    {
+        if (landStatus == LandStatus.Watered)
+        {
+            int hoursElapsed = GameTimestamp.CompareTimestamps(timeWatered, timestamp);
+            Debug.Log(hoursElapsed + "hours since this was watered");
+
+            if (hoursElapsed > 24)
+            {
+                SwitchLandStatus(LandStatus.Farmland);
+            }
+        }
+    }
+}

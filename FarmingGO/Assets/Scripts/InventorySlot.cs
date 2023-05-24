a0b1abf25515aeec00f2b7e427349695878fa01a
Hongshinyoung <gtd4095@naver.com>
2023-05-25 00:25:12 +0900

    inven
    feat: 장착아이템 화면에 표시, 인벤토리 tools, items 버튼으로 각각의 데이터 확인 가능, 인벤토리 첫 번째 칸 = 장착아이템으로 설정
    fix: 인벤토리 UI 버튼 및 배치 수정

--- FarmingGO/Assets/Scripts/InventorySlot.cs
+++ FarmingGO/Assets/Scripts/InventorySlot.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class InventorySlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class InventorySlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     ItemData itemToDisplay;
 
     public Image itemDisplayImage; 
+
+    public enum InventoryType
+    {
+        Item, Tool
+    }
+    //Determines which inventory section this slot is apart of.
+    public InventoryType inventoryType;
+
+    int slotIndex; 
 
     public void Display(ItemData itemToDisplay)
     {
@@ -29,16 +38,28 @@ public class InventorySlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
         
     }
 
+    public virtual void OnPointerClick(PointerEventData eventData)
+    {
+        //Move item from inventory to hand
+        InventoryManager.Instance.InventoryToHand(slotIndex, inventoryType);
+    }
+
+    //Set the Slot index
+    public void AssignIndex(int slotIndex)
+    {
+        this.slotIndex = slotIndex;
+    }
+
 
     //Display the item info on the item info box when the player mouses over
     public void OnPointerEnter(PointerEventData eventData)
     {
-       // UIManager.Instance.DisplayItemInfo(itemToDisplay);
+        // UIManager.Instance.DisplayItemInfo(itemToDisplay);
     }
 
     //Reset the item info box when the player leaves
     public void OnPointerExit(PointerEventData eventData)
     {
-       // UIManager.Instance.DisplayItemInfo(null);
+        // UIManager.Instance.DisplayItemInfo(null);
     }
 }

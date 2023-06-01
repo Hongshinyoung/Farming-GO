bc23071ff0bffc8abb640d99e217ae14dc7ee08f
gdi21 <57336299+gdi21@users.noreply.github.com>
2023-06-01 18:02:46 +0900

    Joystick
    feat: 기존의 조이스틱에서 CharacterController로 이동하는 조이스틱으로 변경, 충돌문제 해결

--- FarmingGO/Assets/Scripts/PlayerController.cs
+++ FarmingGO/Assets/Scripts/PlayerController.cs
@@ -4,19 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public VariableJoystick joy;
     //Movement Components
-    [SerializeField]
-    private Transform characterBody;
-    [SerializeField]
-    private Transform cameraArm;
-    //private CharacterController controller;
+    private CharacterController controller;
     private Animator animator;
 
-    //private float moveSpeed = 4f;
+    private float moveSpeed = 4f;
 
-    //[Header("Movement System")]
-    //public float walkSpeed = 4f;
-    //public float runSpeed = 8f;
+    [Header("Movement System")]
+    public float walkSpeed = 4f;
+    public float runSpeed = 8f;
 
 
     //Interaction components
@@ -26,8 +23,8 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         //Get movement components
-        //controller = GetComponent<CharacterController>();
-        animator = characterBody.GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
 
         //Get interaction component
         playerInteraction = GetComponentInChildren<PlayerInteraction>();
@@ -38,7 +35,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //Runs the function that handles all movement
-        //Move();
+        Move();
 
         //Runs the function that handles all interaction
         Interact();
@@ -67,62 +64,46 @@ public class PlayerController : MonoBehaviour
         //TODO: Set up item interaction
     }
 
-    public void Move(Vector2 inputDirection)
+    public void Move()
     {
-        Vector2 moveInput = inputDirection;
-        bool isMove = moveInput.magnitude != 0;
-        animator.SetBool("isMove", isMove);
-        if (isMove)
+        //Get the horizontal and vertical inputs as a number
+        float horizontal = joy.Horizontal;
+        float vertical = joy.Vertical;
+
+        //Direction in a normalised vector
+        Vector3 dir = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector3 velocity = moveSpeed * Time.deltaTime * dir;
+
+        //Is the sprint key pressed down?
+        if (Input.GetButton("Sprint"))
         {
-            Vector3 lookForward = new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized;
-            Vector3 lookRight = new Vector3(cameraArm.right.x, 0f, cameraArm.right.z).normalized;
-            Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
-
-            characterBody.forward = moveDir;
-            transform.position += moveDir * Time.deltaTime * 5f;
+            //Set the animation to run and increase our movespeed
+            moveSpeed = runSpeed;
+            animator.SetBool("Running", true);
         }
+        else
+        {
+            //Set the animation to walk and decrease our movespeed
+            moveSpeed = walkSpeed;
+            animator.SetBool("Running", false);
+        }
+
+
+        //Check if there is movement
+        if (dir.magnitude >= 0.1f)
+        {
+            //Look towards that direction
+            transform.rotation = Quaternion.LookRotation(dir);
+
+            //Move
+            controller.Move(velocity);
+
+        }
+
+        //Animation speed parameter
+        animator.SetFloat("Speed", velocity.magnitude);
+
+
+
     }
-
-    //public void Move()
-    //{
-    //    //Get the horizontal and vertical inputs as a number
-    //    float horizontal = Input.GetAxisRaw("Horizontal");
-    //    float vertical = Input.GetAxisRaw("Vertical");
-
-    //    //Direction in a normalised vector
-    //    Vector3 dir = new Vector3(horizontal, 0f, vertical).normalized;
-    //    Vector3 velocity = moveSpeed * Time.deltaTime * dir;
-
-    //    //Is the sprint key pressed down?
-    //    if (Input.GetButton("Sprint"))
-    //    {
-    //        //Set the animation to run and increase our movespeed
-    //        moveSpeed = runSpeed;
-    //        animator.SetBool("Running", true);
-    //    }
-    //    else
-    //    {
-    //        //Set the animation to walk and decrease our movespeed
-    //        moveSpeed = walkSpeed;
-    //        animator.SetBool("Running", false);
-    //    }
-
-
-    //    //Check if there is movement
-    //    if (dir.magnitude >= 0.1f)
-    //    {
-    //        //Look towards that direction
-    //        transform.rotation = Quaternion.LookRotation(dir);
-
-    //        //Move
-    //        controller.Move(velocity);
-
-    //    }
-
-    //    //Animation speed parameter
-    //    animator.SetFloat("Speed", velocity.magnitude);
-
-
-
-    //}
 }

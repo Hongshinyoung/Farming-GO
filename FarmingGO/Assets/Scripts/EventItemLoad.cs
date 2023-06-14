a897de33eb313c427b78125e28df3553326be9cd
Hongshinyoung <gtd4095@naver.com>
2023-06-15 00:28:05 +0900

    카메라 이미지 불러오기
    fix: 카메라로 찍은 이미지 불러오기 but 이미지 불러오면 게임 멈춰서 일단 주석처리 하였음.

---
+++ FarmingGO/Assets/Scripts/EventItemLoad.cs
@@ -0,0 +1,60 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.IO;
+//using UnityEngine;
+//using UnityEngine.UI;
+
+//public class EventItemLoad : MonoBehaviour
+//{
+//    public string imagePath = "Assets/ScreenShot/";
+//    [SerializeField]
+//    private RawImage imageRenderer;
+//    private bool isLoaded = false;
+
+//    private void Awake()
+//    {
+//        imageRenderer = GetComponent<RawImage>();
+//        // RawImage 컴포넌트가 할당되었는지 확인
+//        if (imageRenderer == null)
+//        {
+//            Debug.LogError("RawImage 컴포넌트가 할당되지 않았습니다.");
+//            return;
+//        }
+//        LoadAndDisplayImage();
+//    }
+
+//    //public void OnButtonClick()
+//    //{
+//    //    if (isLoaded)
+//    //        return;
+
+//    //    LoadAndDisplayImage();
+//    //    isLoaded = true;
+//    //}
+
+//    private void LoadAndDisplayImage()
+//    {
+//        // Load the PNG files in the specified directory
+//        string[] files = Directory.GetFiles(imagePath, "*.png");
+//        if (files.Length == 0)
+//        {
+//            Debug.Log("No PNG files found in the specified directory.");
+//            return;
+//        }
+
+//        // Sort the files based on their names (assuming the names are numbers)
+//        System.Array.Sort(files);
+
+//        // Get the first PNG file in the directory
+//        string filePath = files[0];
+
+//        byte[] bytes = File.ReadAllBytes(filePath);
+
+//        // Create a Texture2D and load the image bytes
+//        Texture2D texture = new Texture2D(2, 2);
+//        texture.LoadImage(bytes);
+
+//        // Assign the texture to the RawImage component
+//        imageRenderer.texture = texture;
+//    }
+//}

f92e90c3f83627416cc246663c98704a27bc12db
rbs611 <116694317+rbs611@users.noreply.github.com>
2023-06-15 17:42:12 +0900

    ShopFix
    fix: 상점 상호작용 가능

--- FarmingGO/Assets/Scripts/UI/ShopListingManager.cs
+++ FarmingGO/Assets/Scripts/UI/ShopListingManager.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopListingManager : MonoBehaviour
 {
     public GameObject shopLisiting;
     public Transform listingGrid;
+
+    ItemData itemToBuy;
+    int quantity;
+
+    [Header("Confirmation Screen")]
+    public GameObject confirmationScreen;
+    public Text confirmationPrompt;
+    public Text quantityText;
+    public Text costCalculationText;
+    public Button purchaseButton;
 
     public void RenderShop(List<ItemData> shopItems)
     {
@@ -23,5 +34,62 @@ public class ShopListingManager : MonoBehaviour
 
             listingGameObject.GetComponent<ShopListing>().Display(shopItem);
         }
+    }
+
+    public void OpenConfirmationScreen(ItemData item)
+    {
+        itemToBuy = item;
+        quantity = 1;
+        RenderConfirmationScreen();
+    }
+
+    public void RenderConfirmationScreen()
+    {
+        confirmationScreen.SetActive(true);
+
+        confirmationPrompt.text = $"Buy {itemToBuy.name}?";
+
+        quantityText.text = "x" + quantity;
+
+        int cost = itemToBuy.cost * quantity;
+
+        int playerMoneyLeft = PlayerStats.Money - cost;
+
+        if(playerMoneyLeft < 0)
+        {
+            costCalculationText.text = "Insufficient funds";
+            purchaseButton.interactable = false;
+            return;
+        }
+
+        purchaseButton.interactable = true;
+
+        costCalculationText.text = $"{PlayerStats.Money} > {playerMoneyLeft}";
+    }
+
+    public void AddQuantity()
+    {
+        quantity++;
+        RenderConfirmationScreen();
+    }
+
+    public void SubtractQuantity()
+    {
+        if(quantity > 1)
+        {
+            quantity--;
+        }
+        RenderConfirmationScreen();
+    }
+
+    public void ConfirmPurchase()
+    {
+        Shop.Purchase(itemToBuy, quantity);
+        confirmationScreen.SetActive(false);
+    }
+
+    public void CancelPurchase()
+    {
+        confirmationScreen.SetActive(false);
     }
 }

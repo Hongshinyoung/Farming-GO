188da401fcdae64be5ff91720f0e6959614134a7
rbs611 <116694317+rbs611@users.noreply.github.com>
2023-10-30 21:26:41 +0900

    feat: Titel Screen
    타이틀 스크린 추가

---
+++ FarmingGO/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame()
+    {
+        //SceneManger.LoadScene(SceneTransitiomManager.Location.PlayerHome.ToString());
+    }
+    
+    public void ContinueGame()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

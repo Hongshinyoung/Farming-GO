1a67318f91db10c85edc3dbaa3683cd0bfa817cd
Hongshinyoung <88022654+Hongshinyoung@users.noreply.github.com>
2023-11-15 22:34:49 +0900

    퀘스트
    fix: 퀘스트 수정(퀘스트2 null버그 있음.)

--- FarmingGO/Assets/Scripts/Quest/QuestComplete.cs
+++ FarmingGO/Assets/Scripts/Quest/QuestComplete.cs
@@ -5,44 +5,26 @@ using UnityEngine;
 public class QuestComplete : MonoBehaviour
 {
     public InventoryManager inventoryManager;
+    public Land land;
     public ItemData questTool;
     public TextMeshProUGUI questComplete;
 
     public bool isComplete;
 
     private QuestLoader loader;
-    private Land land;
 
     private void Start()
     {
         loader = GetComponentInParent<QuestLoader>();
-        loader.btn_Yes.onClick.AddListener(Update);
+        land = GetComponent<Land>();
+        
     }
-
     private void Update()
     {
-        //첫 번째 퀘스트 로직
-
-        if (!isComplete && inventoryManager != null)
-        {
-            if (inventoryManager.GetEquippedSlot(InventorySlot.InventoryType.Tool).itemData == questTool)
-            {
-                isComplete = true;
-                StartCoroutine(ActiveQuestComplete());
-            }
-        }
-
-
-        //두 번째 퀘스트 로직
-        if (!isComplete)
-        {
-            if (CompareTag("Land") && land.landStatus == Land.LandStatus.Farmland)
-            {
-                isComplete = true;
-                StartCoroutine(ActiveQuestComplete());
-            }
-        }
+        FirstQuest();
+        SecondQuest();
     }
+
     // 퀘스트 클리어 시 클리어 문구 1초간 표시
     IEnumerator ActiveQuestComplete()
     {
@@ -59,5 +41,34 @@ public class QuestComplete : MonoBehaviour
         {
             loader.MoveToNextQuest();
         }
+    }
+
+    void FirstQuest()
+    {
+        //첫 번째 퀘스트 로직
+
+        if (!isComplete && inventoryManager != null)
+        {
+            if (inventoryManager.GetEquippedSlot(InventorySlot.InventoryType.Tool).itemData == questTool)
+            {
+                isComplete = true;
+                StartCoroutine(ActiveQuestComplete());
+            }
+        }
+    }
+
+    void SecondQuest() 
+    {
+        //두 번째 퀘스트 로직
+        if (!isComplete && land != null)
+        {
+            // Land 프리팹의 LandStatus가 Farmland로 바뀌면 퀘스트 성공
+            if (land.landStatus == Land.LandStatus.Farmland)
+            {
+                isComplete = true;
+                StartCoroutine(ActiveQuestComplete());
+            }
+        }
+        else Debug.Log("land is null");
     }
 }
